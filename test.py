6cfb8ec349dd66b0d0ebe2cfcddd50f777a9264a
v.stadnytskyi@gmail.com <v.stadnytskyi@gmail.com>
2019-09-06 09:44:26 -0400

    improved test.py file

--- test.py
+++ test.py
@@ -1,65 +1,54 @@
 """
 numpy testing
 """
-import os
-import timeit
-os.path.isdir('/Users/femto-13/All-Projects-on-femto/LaserLab/Software/numpy_testing/')
-root = '/Users/femto-13/All-Projects-on-femto/LaserLab/Software/numpy_testing/'
-folder = 'xray_images/'
 
-lst = os.listdir(folder)
-import sys
-sys.path.append('/Users/femto-13/All-Projects-on-femto/APS/Instrumentation/Software/Lauecollect/')
-from PIL import Image
-from numpy import array
+class Test(object):
+    def __init__(self):
+        pass
+    
+    def generate_data(self):
+        """
+        generate 32 image, size = 3960x3960, values = random numbers, dtype= uint16.
+        """
+        from numpy import array, random
+        lst = range(32)
 
-def import_images():
-    from PIL import Image
-    from numpy import array
-    import os
-    global folder
-    lst = os.listdir(folder)
-    data = []
-    i = 0
-    for item in lst:
-        data.append(array(Image.open(folder+item)))
-        i+=1
-    return array(data)
-data = import_images()
+        i = 0
+        self.data = random.randint(2**16-1, size=(3960, 3960),dtype = 'uint16')
 
-def sum_axis0():
-    global data
-    return data.sum(axis = 0)
 
-def sum_axis1():
-    global data
-    return data.sum(axis = 1)
+    def test_sum_axis0(self):
+        return self.data.sum(axis = 0)
 
-def sum_full():
-    global data
-    return data.sum()
+    def test_sum_axis1(self):
+        return self.data.sum(axis = 1)
 
-def mean():
-    global data
-    return data.mean()
+    def test_sum_full(self):
+        return self.data.sum()
 
-if __name__ is "__main__":
-    N = 5
-    res = timeit.timeit(stmt = import_images, setup = '', number = N)
-    print("read 32 images N = {} times: {} seconds per loop".format(N,res/N))
+    def test_mean(self):
+        return self.data.mean()
 
-    N = 20
-    res = timeit.timeit(stmt = sum_axis0, setup = '', number = N)
-    print("sum axis=0 32 images N = {} times: {} seconds per loop".format(N,res/N))
+if __name__ == "__main__":
+    import timeit
+    test = Test()
+    test.generate_data()
+    N = 50
+    res = timeit.timeit(stmt = test.generate_data, setup = '', number = N)
+    print("generate_data, N times = {} times: {} seconds per loop".format(N,res/N))
 
-    N = 20
-    res = timeit.timeit(stmt = sum_axis1, setup = '', number = N)
-    print("sum axis=1 32 images N = {} times: {} seconds per loop".format(N,res/N))
+    N = 200
+    res = timeit.timeit(stmt = test.test_sum_axis0, setup = '', number = N)
+    print("sum axis=0 N times= {} times: {} seconds per loop".format(N,res/N))
 
-    N = 20
-    res = timeit.timeit(stmt = sum_full, setup = '', number = N)
-    print("sum full 32 images N = {} times: {} seconds per loop".format(N,res/N))
+    N = 200
+    res = timeit.timeit(stmt = test.test_sum_axis1, setup = '', number = N)
+    print("sum axis=1 N times = {} times: {} seconds per loop".format(N,res/N))
 
-    N = 20
-    res = timeit.timeit(stmt = mean, setup = '', number = N)
-    print("mean 32 images N = {} times: {} seconds per loop".format(N,res/N))
+    N = 200
+    res = timeit.timeit(stmt = test.test_sum_full, setup = '', number = N)
+    print("sum full N times= {} times: {} seconds per loop".format(N,res/N))
+
+    N = 200
+    res = timeit.timeit(stmt = test.test_mean, setup = '', number = N)
+    print("mean 32 N times = {} times: {} seconds per loop".format(N,res/N))
